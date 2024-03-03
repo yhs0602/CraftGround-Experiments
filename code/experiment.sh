0152257edbe66dd36abe0bb52e93cabc5a34faf6
yhs0602 <jourhyang123@gmail.com>
2024-03-03 23:20:38 +0900

    :alembic: Experiments

---
+++ code/experiment.sh
@@ -0,0 +1,19 @@
+# zsh
+python runner.py --config experiments/kcc/husk-vision-dqn.yml  || true
+python runner.py --config experiments/kcc/husk-vision-dqn.yml  || true
+
+python runner.py --config experiments/kcc/husk-vision-drqn.yml || true
+python runner.py --config experiments/kcc/husk-vision-drqn.yml || true
+
+python runner.py --config experiments/kcc/husk-vision-stack-dqn.yml || true
+python runner.py --config experiments/kcc/husk-vision-stack-dqn.yml || true
+
+python runner.py --config experiments/kcc/husk-bimodal-dqn.yml || true
+python runner.py --config experiments/kcc/husk-bimodal-dqn.yml || true
+
+python runner.py --config experiments/kcc/husk-bimodal-drqn.yml || true
+python runner.py --config experiments/kcc/husk-bimodal-drqn.yml || true
+
+python runner.py --config experiments/kcc/husk-bimodal-stack-dqn.yml || true
+python runner.py --config experiments/kcc/husk-bimodal-stack-dqn.yml || true
+
